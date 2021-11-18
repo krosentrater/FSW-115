8c5996acacc15da09c04e317124054291e49cf5e
krosentrater <87054047+krosentrater@users.noreply.github.com>
2021-11-17 17:12:11 -0800

    Axios Todo - Part 1
    Axios Todo - Part 1

---
+++ Assignment 1/main.js
@@ -0,0 +1,16 @@
+// http://api.bryanuniversity.edu/kr/list/
+
+// Get All (returns array)
+
+axios.get('http://api.bryanuniversity.edu/kr/list/')
+.then(response => {
+    for (let i = 0; i < response.data.length; i++){
+        const h1 = document.createElement('h1');
+        h1.textContent = response.data[i].name;
+        document.body.appendChild(h1);
+    };
+})
+.catch(error => console.log(error))
+
+
+// Get One (returns object)
